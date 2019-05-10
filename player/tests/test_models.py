e7738f8d96eae57a41b3034047a62ed8bd881515
Brian Perrett <perrettbrian@gmail.com>
2019-05-09 20:48:12 -0700

    add testing for player models

--- player/tests/test_models.py
+++ player/tests/test_models.py
@@ -3,12 +3,18 @@
 Tests for player models.
 
 """
-from mock import Mock
+# STANDARD LIBRARY
+from unittest.mock import Mock
+
+# 3RD PARTY
 from model_mommy import mommy 
 
+# STANDARD DJANGO
 from django.test import TestCase
+from django.utils import timezone
 
-from player.models import simplify
+# LOCAL DJANGO
+from player.models import simplify, NameChange
 
 
 class TestSimplify(TestCase):
@@ -23,3 +29,84 @@ class TestSimplify(TestCase):
         )
         for name in names:
             self.assertEqual(simplify(name[0]), name[1])
+
+
+class SummonerTest(TestCase):
+
+    def test_save(self):
+        summoner = mommy.make(
+            'player.Summoner',
+            name='',
+        )
+        self.assertEqual(summoner.name, '')
+
+    def test_str(self):
+        summoner = mommy.make(
+            'player.Summoner',
+            name='hello world',
+            region='na',
+        )
+        self.assertEqual(
+            summoner.__str__(),
+            f'Summoner(name="hello world", region=na)'
+        )
+
+    def test_name_change_creation_on_save(self):
+        namechange_count = NameChange.objects.all().count()
+        self.assertEqual(namechange_count, 0)
+
+        summoner = mommy.make(
+            'player.Summoner',
+            name='name numbah 1',
+        )
+        summoner.name = 'name numbah 2'
+        summoner.save()
+
+        namechange_count = NameChange.objects.all().count()
+        self.assertEqual(namechange_count, 1)
+
+    def test_get_newest_rank_checkpoint(self):
+        summoner = mommy.make('player.Summoner')
+
+        self.assertEqual(summoner.get_newest_rank_checkpoint(), None)
+
+        old_rank_checkpoint = mommy.make(
+            'player.RankCheckpoint',
+            summoner=summoner,
+            created_date=timezone.now() - timezone.timedelta(days=300),
+        )
+        self.assertEqual(summoner.get_newest_rank_checkpoint(), old_rank_checkpoint)
+
+        new_rank_checkpoint = mommy.make(
+            'player.RankCheckpoint',
+            summoner=summoner,
+            created_date=timezone.now(),
+        )
+        self.assertEqual(summoner.get_newest_rank_checkpoint(), new_rank_checkpoint)
+
+
+class NameChangeTest(TestCase):
+
+    def test_str(self):
+        namechange = mommy.make(
+            'player.NameChange',
+            old_name='oldboy',
+            summoner=mommy.make(
+                'player.Summoner',
+                name='newboy',
+            ),
+        )
+        self.assertEqual(
+            namechange.__str__(),
+            f'NameChange(old_name="oldboy", new_name="newboy")'
+        )
+
+
+class CustomTest(TestCase):
+
+    def test_save(self):
+        before = timezone.now()
+        custom = mommy.make('player.Custom')
+        after = timezone.now()
+        self.assertGreater(custom.modified_date, before)
+        self.assertGreater(after, custom.modified_date)
