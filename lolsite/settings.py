b6b390a8ef6bcf9bd7a4245908d7dfe081e18a48
Brian Perrett <perrettbrian@gmail.com>
2019-06-16 00:28:10 -0700

    bug fix

--- lolsite/settings.py
+++ lolsite/settings.py
@@ -227,11 +227,11 @@ elif not DEV:
     # s3 static settings
     STATICFILES_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    STATICFILES_STORAGE = 'lolsite.custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
     MEDIAFILES_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'lolsite.custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 
 # CELERY SETTINGS
