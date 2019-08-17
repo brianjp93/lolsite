7de34469661be7fdcaef2727eff2e073efa8cae4
Brian Perrett <perrettbrian@gmail.com>
2019-08-17 16:29:08 -0600

    allow queue selection for rank history graph

--- react/src/components/summoner/RankHistory.jsx
+++ react/src/components/summoner/RankHistory.jsx
@@ -7,6 +7,7 @@ import moment from 'moment'
 
 function RankHistory(props) {
     const [rank_data, setRankData] = useState([])
+    const [show_queue, setShowQueue] = useState('RANKED_SOLO_5x5')
     const [modified_rank_data, setModifiedRankData] =  useState([])
     const [top_master_lp, setTopMasterLP] = useState(100)
     const [top_grandmaster_lp, setTopGrandMasterLP] = useState(100)
@@ -68,7 +69,7 @@ function RankHistory(props) {
         }
         else if (one_div_tiers.indexOf(tier.toUpperCase()) >= 0) {
             let index = one_div_tiers.indexOf(tier.toUpperCase())
-            let div_number = (four_div_tiers.length * 4) + (index * 4) - 4
+            let div_number = (four_div_tiers.length * 4) + (index * 4)
             let lp_number = (lp / top_rank_lp[tier.toLowerCase()].get) * 4
             num = div_number + lp_number
         }
@@ -80,7 +81,7 @@ function RankHistory(props) {
 
         let data = {
             id: props.summoner.id,
-            queue: 'RANKED_SOLO_5x5',
+            queue: show_queue,
             group_by: 'day',
         }
         api.player.getRankHistory(data)
@@ -88,7 +89,7 @@ function RankHistory(props) {
                 let d = response.data.data
                 setRankData(d)
             })
-    }, [props.summoner])
+    }, [props.summoner, show_queue])
     // console.log(rank_data)
 
     // SET TOP LP VALUES TO CORRECTLY SCALE
@@ -155,58 +156,79 @@ function RankHistory(props) {
 
     return (
         <div>
-            <AreaChart
-                margin={{left: 0, top: 5}}
-                width={700}
-                height={200}
-                data={modified_rank_data}>
 
-                <XAxis
-                    tickFormatter={(tick) => {
-                        var m = moment(tick)
-                        return `${m.format('ll')}`
-                    }}
-                    dataKey='epoch' />
+            <div style={{display: 'inline-block', verticalAlign: 'top'}}>
+                <label htmlFor={`soloq-rank-selection`}>
+                    <input id={`soloq-rank-selection`} onChange={useCallback(() => setShowQueue('RANKED_SOLO_5x5'), [])} type="radio" checked={show_queue === 'RANKED_SOLO_5x5'}/>
+                    <span>Solo/Duo</span>
+                </label>
+                <div style={{width: 10}}></div>
+                <label htmlFor={`flex-rank-selection`}>
+                    <input id={`flex-rank-selection`} onChange={useCallback(() => setShowQueue('RANKED_FLEX_SR'), [])} type="radio" checked={show_queue === 'RANKED_FLEX_SR'}/>
+                    <span>Flex</span>
+                </label>
+                <div style={{width: 10}}></div>
+                <label htmlFor={`tft-rank-selection`}>
+                    <input id={`tft-rank-selection`} onChange={useCallback(() => setShowQueue('RANKED_TFT'), [])} type="radio" checked={show_queue === 'RANKED_TFT'}/>
+                    <span>TFT</span>
+                </label>
+            </div>
 
-                {/*
-                */}
-                <YAxis
-                    tick={false}
-                    domain={['dataMin', 'dataMax']} />
-                <Area dataKey='numeric_rank_range' />
+            <div style={{display: 'inline-block'}}>
+                <AreaChart
+                    margin={{left: -20, top: 5}}
+                    width={650}
+                    height={200}
+                    data={modified_rank_data}>
 
-                {[...four_div_tiers, ...one_div_tiers].map((tier, key) => {
-                    if (key > 0) {
+                    <XAxis
+                        tickFormatter={(tick) => {
+                            var m = moment(tick)
+                            return `${m.format('ll')}`
+                        }}
+                        dataKey='epoch' />
+
+                    {/*
+                    */}
+                    <YAxis
+                        tick={false}
+                        domain={['dataMin', 'dataMax']} />
+                    <Area dataKey='numeric_rank_range' />
+
+                    {[...four_div_tiers, ...one_div_tiers].map((tier, key) => {
+                        if (key > 0) {
+                            return (
+                                <ReferenceLine key={key} y={4 * (key)} label={`${tier}`} stroke="#ffffff" />
+                            )
+                        }
+                        return null
+                    })}
+
+                    {four_div_tiers.map((tier, key) => {
                         return (
-                            <ReferenceLine key={key} y={4 * (key)} label={`${tier}`} stroke="#ffffff" />
+                            [
+                                <ReferenceLine key={`${key}-1`} y={(4 * key) + 1} stroke="#ffffff20" strokeDasharray="3 3" />,
+                                <ReferenceLine key={`${key}-2`} y={(4 * key) + 2} stroke="#ffffff20" strokeDasharray="3 3" />,
+                                <ReferenceLine key={`${key}-3`} y={(4 * key) + 3} stroke="#ffffff20" strokeDasharray="3 3" />
+                            ]
                         )
-                    }
-                    return null
-                })}
+                    })}
 
-                {four_div_tiers.map((tier, key) => {
-                    return (
-                        [
-                            <ReferenceLine key={`${key}-1`} y={(4 * key) + 1} stroke="#ffffff20" strokeDasharray="3 3" />,
-                            <ReferenceLine key={`${key}-2`} y={(4 * key) + 2} stroke="#ffffff20" strokeDasharray="3 3" />,
-                            <ReferenceLine key={`${key}-3`} y={(4 * key) + 3} stroke="#ffffff20" strokeDasharray="3 3" />
-                        ]
-                    )
-                })}
-
-                <Tooltip
-                    formatter={(value, name, props) => {
-                        // console.log(value)
-                        let peak = props.payload.peak_rank
-                        let trough = props.payload.trough_rank
-                        let output = `${trough.tier.toUpperCase()} ${trough.division} ${trough.league_points} -to- ${peak.tier.toUpperCase()} ${peak.division} ${peak.league_points}`
-                        return [output, 'Rank']
-                    }}
-                    labelFormatter={(label) => {
-                        var m = moment(label)
-                        return `${m.format('ll')}`
-                    }}  />
-            </AreaChart>
+                    <Tooltip
+                        formatter={(value, name, props) => {
+                            // console.log(value)
+                            let peak = props.payload.peak_rank
+                            let trough = props.payload.trough_rank
+                            let output = `${trough.tier.toUpperCase()} ${trough.division} ${trough.league_points} -to- ${peak.tier.toUpperCase()} ${peak.division} ${peak.league_points}`
+                            return [output, 'Rank']
+                        }}
+                        labelFormatter={(label) => {
+                            var m = moment(label)
+                            return `${m.format('ll')}`
+                        }}  />
+                </AreaChart>
+                
+            </div>
         </div>
     )
 }
