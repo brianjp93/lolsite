ee790a973bad32c07f9d6030f2731d382d3e63de
Brian Perrett <perrettbrian@gmail.com>
2020-07-17 15:53:08 -0600

    am dumb

--- player/admin.py
+++ player/admin.py
@@ -6,7 +6,7 @@ from .models import Custom, EmailVerification, SummonerLink
 
 class SummonerAdmin(admin.ModelAdmin):
     list_display = ("simple_name", "region")
-    search_fields = ("simple_name")
+    search_fields = ("simple_name", )
     list_select_related = True
     raw_id_fields = ("user", "pro")
     show_full_result_count = False
