b2807c183bb8906431f347a7cd778092e6072862
Brian Perrett <perrettbrian@gmail.com>
2020-07-23 00:05:12 -0600

    prettify file

--- react/src/components/summoner/ChampionTimelines.jsx
+++ react/src/components/summoner/ChampionTimelines.jsx
@@ -1,23 +1,38 @@
 import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
-import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 import numeral from 'numeral'
 
 // MAIN
 function ChampionTimelines(props) {
-    const [participant_selection, setParticipantSelection] = useState(props.participants.map(item => item._id))
+    const [participant_selection, setParticipantSelection] = useState(
+        props.participants.map(item => item._id),
+    )
     const [graph_type, setGraphType] = useState('total_gold')
 
     const image_width = 30
     const usable_width = props.expanded_width - 30
-    const available_width = (usable_width - (props.participants.length * image_width))
+    const available_width = usable_width - props.participants.length * image_width
     const padding_pixels = available_width / props.participants.length
     const participants = [
         ...props.participants.filter(participant => participant.team_id === 100),
-        ...props.participants.filter(participant => participant.team_id === 200)
+        ...props.participants.filter(participant => participant.team_id === 200),
     ]
     const participant_ids = participants.map(participant => participant._id)
-    const colors = ['#d94630', '#d98d30', '#d9d330', '#90d930' ,'#3ca668', '#3ca69a', '#3c71a6', '#545acc', '#8c4fd6', '#d24fd6', '#f55fa0', '#994352']
+    const colors = [
+        '#d94630',
+        '#d98d30',
+        '#d9d330',
+        '#90d930',
+        '#3ca668',
+        '#3ca69a',
+        '#3c71a6',
+        '#545acc',
+        '#8c4fd6',
+        '#d24fd6',
+        '#f55fa0',
+        '#994352',
+    ]
 
     return (
         <div>
@@ -36,19 +51,24 @@ function ChampionTimelines(props) {
                             let new_selection = [...participant_selection]
                             if (participant_selection.indexOf(participant._id) >= 0) {
                                 new_selection = new_selection.filter(id => id !== participant._id)
-                            }
-                            else {
+                            } else {
                                 new_selection.push(participant._id)
                             }
-                            setParticipantSelection(new_selection)}} />
+                            setParticipantSelection(new_selection)
+                        }}
+                    />
                 )
             })}
 
-            <div className='row' style={{marginLeft: 0, marginRight: 0}}>
+            <div className="row" style={{ marginLeft: 0, marginRight: 0 }}>
                 <div className="col s6">
                     <button
                         onClick={useCallback(() => setParticipantSelection([]), [])}
-                        style={{width: '100%'}} className={`${props.theme} btn-small`}>Clear All</button>
+                        style={{ width: '100%' }}
+                        className={`${props.theme} btn-small`}
+                    >
+                        Clear All
+                    </button>
                 </div>
                 <div className="col s6">
                     <button
@@ -56,35 +76,42 @@ function ChampionTimelines(props) {
                             let parts = participants.map(part => part._id)
                             setParticipantSelection(parts)
                         }, [participants])}
-                        style={{width: '100%'}} className={`${props.theme} btn-small`}>Select All</button>
+                        style={{ width: '100%' }}
+                        className={`${props.theme} btn-small`}
+                    >
+                        Select All
+                    </button>
                 </div>
             </div>
-            
+
             <div>
                 <LineChart
                     margin={{
-                      left: -10, right: 20,
+                        left: -10,
+                        right: 20,
                     }}
-                    width={usable_width} height={400} data={props.timeline}>
-                    <CartesianGrid
-                        vertical={false}
-                        stroke='#777'
-                        strokeDasharray="4 4" />
+                    width={usable_width}
+                    height={400}
+                    data={props.timeline}
+                >
+                    <CartesianGrid vertical={false} stroke="#777" strokeDasharray="4 4" />
 
                     <XAxis
                         hide={false}
-                        tickFormatter={(tickItem) => {
+                        tickFormatter={tickItem => {
                             var m = Math.round(tickItem / 1000 / 60)
                             return `${m}m`
                         }}
-                        dataKey="timestamp" />
+                        dataKey="timestamp"
+                    />
 
                     <YAxis
-                        yAxisId='left'
-                        orientation='left'
-                        tickFormatter={(tick) => {
+                        yAxisId="left"
+                        orientation="left"
+                        tickFormatter={tick => {
                             return numeral(tick).format('0a')
-                        }} />
+                        }}
+                    />
 
                     {participant_selection.map(id => {
                         let stroke = colors[participant_ids.indexOf(id)]
@@ -101,10 +128,10 @@ function ChampionTimelines(props) {
                                 name={getParticipant(props.participants, id).champion.name}
                                 key={`${id}-line-chart`}
                                 isAnimationActive={false}
-                                yAxisId='left'
+                                yAxisId="left"
                                 type={stroke_type}
                                 dot={false}
-                                dataKey={(frame) => {
+                                dataKey={frame => {
                                     for (let part of frame.participantframes) {
                                         if (part.participant_id === id) {
                                             return part[graph_type]
@@ -113,61 +140,79 @@ function ChampionTimelines(props) {
                                     return null
                                 }}
                                 stroke={stroke}
-                                strokeWidth={stroke_width} />
+                                strokeWidth={stroke_width}
+                            />
                         )
                     })}
                     <Tooltip
                         itemSorter={item => -item.value}
-                        wrapperStyle={{zIndex: 10}}
+                        wrapperStyle={{ zIndex: 10 }}
                         formatter={(value, name, props) => {
                             let output
                             if (graph_type === 'total_gold') {
                                 output = `${numeral(value).format('0,0')} gold`
-                            }
-                            else {
+                            } else {
                                 output = value
                             }
                             return output
                         }}
-                        labelFormatter={(label) => {
+                        labelFormatter={label => {
                             var m = Math.round(label / 1000 / 60)
                             return `${m}m`
-                        }} />
-                    
+                        }}
+                    />
                 </LineChart>
             </div>
 
-            <div className='row' style={{marginLeft: 20, marginRight: 15}}>
+            <div className="row" style={{ marginLeft: 20, marginRight: 15 }}>
                 <div className="col s3">
                     <label htmlFor="gold-champion-graph">
-                        <input id='gold-champion-graph' onChange={() => setGraphType('total_gold')} type="radio" checked={graph_type === 'total_gold'}/>
+                        <input
+                            id="gold-champion-graph"
+                            onChange={() => setGraphType('total_gold')}
+                            type="radio"
+                            checked={graph_type === 'total_gold'}
+                        />
                         <span>Gold</span>
                     </label>
                 </div>
 
                 <div className="col s3">
                     <label htmlFor="cs-champion-graph">
-                        <input id='cs-champion-graph' onChange={() => setGraphType('cs')} type="radio" checked={graph_type === 'cs'}/>
+                        <input
+                            id="cs-champion-graph"
+                            onChange={() => setGraphType('cs')}
+                            type="radio"
+                            checked={graph_type === 'cs'}
+                        />
                         <span>CS</span>
                     </label>
                 </div>
 
                 <div className="col s3">
                     <label htmlFor="xp-champion-graph">
-                        <input id='xp-champion-graph' onChange={() => setGraphType('xp')} type="radio" checked={graph_type === 'xp'}/>
+                        <input
+                            id="xp-champion-graph"
+                            onChange={() => setGraphType('xp')}
+                            type="radio"
+                            checked={graph_type === 'xp'}
+                        />
                         <span>XP</span>
                     </label>
                 </div>
 
                 <div className="col s3">
                     <label htmlFor="level-champion-graph">
-                        <input id='level-champion-graph' onChange={() => setGraphType('level')} type="radio" checked={graph_type === 'level'}/>
+                        <input
+                            id="level-champion-graph"
+                            onChange={() => setGraphType('level')}
+                            type="radio"
+                            checked={graph_type === 'level'}
+                        />
                         <span>Level</span>
                     </label>
                 </div>
-
             </div>
-
         </div>
     )
 }
@@ -198,21 +243,22 @@ function ChampionImage(props) {
     if (!props.is_selected) {
         image_style = {
             ...image_style,
-            opacity: .3,
+            opacity: 0.3,
         }
     }
     return (
-        <div style={{display: 'inline-block', paddingRight: props.padding_pixels}}>
+        <div style={{ display: 'inline-block', paddingRight: props.padding_pixels }}>
             <img
                 onClick={props.handleClick}
                 style={{
                     cursor: 'pointer',
                     width: 30,
-                    ...image_style
+                    ...image_style,
                 }}
                 aria-label={props.participant.champion.name}
                 src={props.participant.champion.image_url}
-                alt=""/>
+                alt=""
+            />
         </div>
     )
 }
