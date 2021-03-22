da464285731c2bd95c0835da7b34b8b65c20b7a9
Brian Perrett <bperrett@fusionbox.com>
2021-03-22 12:56:44 -0600

    Edit fish command to work on mac.
    - Remove subprocess from rundev command

--- start.fish
+++ start.fish
@@ -5,8 +5,10 @@
 # postgres and redis-server must be running
 # create session `lolsite`
 # assumes venv `lolsite`
-set path (pwd)
-tmux new-session -c $path -d -s lolsite 'conda activate lolsite && python manage.py rundev'
-tmux split-window -c $path -h 'conda activate lolsite && python manage.py celery'
-tmux split-window -c "$path/react/" -v 'npm run start'
+set environ 'conda deactivate && conda activate lolsite'
+tmux new-session -d -s lolsite
+tmux send-keys "$environ && python manage.py rundev" C-m
+tmux split-window -h
+tmux send-keys "$environ && python manage.py celery" C-m
+tmux split-window -c "./react/" -v 'npm run start'
 tmux a -t lolsite
