742d743020a126ad57f0a42acde7ad7beb84428d
Brian Perrett <perrettbrian@gmail.com>
2021-03-26 01:02:57 -0600

    Move some styles into index.css

--- react/src/components/general/StatBar.jsx
+++ react/src/components/general/StatBar.jsx
@@ -4,7 +4,6 @@ import ReactTooltip from 'react-tooltip'
 import uuidv4 from 'uuid/v4'
 import numeral from 'numeral'
 
-
 class StatBar extends Component {
     constructor(props) {
         super(props)
@@ -19,8 +18,7 @@ class StatBar extends Component {
         let total = this.getTotal()
         if (val === 1) {
             out = (this.props.val1 / total) * 100
-        }
-        else {
+        } else {
             out = (this.props.val2 / total) * 100
         }
         return out
@@ -32,23 +30,9 @@ class StatBar extends Component {
         const id1 = uuidv4()
         const id2 = uuidv4()
 
-        const color1 = {
-            background: 'linear-gradient(81deg, rgba(40,212,180,1) 0%, rgb(58, 128, 157) 100%)',
-        }
-        const color2 = {
-            background: 'linear-gradient(81deg, rgb(230, 136, 103) 0%, rgba(199,37,37,1) 100%)',
-        }
-
-        const border_radius = 5
-        const barstyle = {
-            height: 15,
-            display: 'inline-block',
-        }
         return (
-            <div style={{display: 'inline-block', width: '100%'}}>
-                <ReactTooltip
-                    id={id1}
-                    effect='solid' >
+            <div style={{ display: 'inline-block', width: '100%' }}>
+                <ReactTooltip id={id1} effect="solid">
                     <span>
                         {numeral(this.getPercentage(1)).format('0.0')}%: {this.props.label1}
                     </span>
@@ -56,31 +40,21 @@ class StatBar extends Component {
                 <div
                     data-tip
                     data-for={id1}
-                    style={{
-                        ...barstyle,
-                        ...color1,
-                        width: `${this.getPercentage(1)}%`,
-                        borderTopLeftRadius: border_radius,
-                        borderBottomLeftRadius: border_radius,
-                    }}>
-                </div>
+                    className="winning-bar"
+                    style={{ width: `${this.getPercentage(1)}%` }}
+                ></div>
 
-                <ReactTooltip
-                    id={id2}
-                    effect='solid' >
-                    <span>{numeral(this.getPercentage(2)).format('0.0')}%: {this.props.label2}</span>
+                <ReactTooltip id={id2} effect="solid">
+                    <span>
+                        {numeral(this.getPercentage(2)).format('0.0')}%: {this.props.label2}
+                    </span>
                 </ReactTooltip>
                 <div
                     data-tip
                     data-for={id2}
-                    style={{
-                        ...barstyle,
-                        ...color2,
-                        width: `${this.getPercentage(2)}%`,
-                        borderTopRightRadius: border_radius,
-                        borderBottomRightRadius: border_radius,
-                    }}>
-                </div>
+                    className="losing-bar"
+                    style={{ width: `${this.getPercentage(2)}%` }}
+                ></div>
             </div>
         )
     }
