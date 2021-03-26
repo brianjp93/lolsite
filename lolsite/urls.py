4455bcea1f1490f48b843a90ab8e66d1f68fc2a3
Brian Perrett <perrettbrian@gmail.com>
2021-03-26 02:01:29 -0600

    Save champion images into media files (s3)

--- lolsite/urls.py
+++ lolsite/urls.py
@@ -17,12 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from player import views as player_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("lolsite.urlsapi")),
     path("login/go/", player_views.login_action),
     path("logout/", player_views.logout_action, name="logout"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path("<path:path>/", views.home),
     path("", views.home, name="home"),
 ]
