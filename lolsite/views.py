915f6e3c1063dd82f66078a32067125210aebb16
Brian Perrett <perrettbrian@gmail.com>
2021-03-28 17:35:56 -0600

    Show some more interesting data with link preview.

--- lolsite/views.py
+++ lolsite/views.py
@@ -4,12 +4,16 @@ from django.template.response import TemplateResponse
 from django.templatetags.static import static
 
 from player.serializers import FavoriteSerializer, SummonerSerializer
+from player.models import Summoner, simplify
+
+from match.models import Match
 
 from data import tasks as dt
 from data import constants
 
 from notification import tasks as nt
 
+import re
 import json
 import logging
 
@@ -38,6 +42,52 @@ def get_meta_data(request):
         'image': logo,
         'description': 'Accept your hardstuck-ness.',
     }
+    r = re.match(r'/([a-z]+)/(.*)/', request.path)
+    if r:
+        region, name = r.groups()
+        print(region, name)
+        name = simplify(name)
+        qs = Summoner.objects.filter(region=region, simple_name=name)
+        wins = 0
+        losses = 0
+        champions = {}
+        if qs:
+            summoner = qs[0]
+            matches = Match.objects.filter(
+                participants__account_id=summoner.account_id,
+                game_duration__gt=600,
+            ).order_by('-game_creation')[:20]
+            for match in matches:
+                part = match.participants.get(account_id=summoner.account_id)
+                is_win = part.stats.win
+
+                # overall stats
+                if is_win:
+                    wins += 1
+                else:
+                    losses += 1
+
+                # per champ stats
+                champ = part.get_champion()
+                if champ:
+                    newstat = champions[champ.name] = champions.get(champ.name, {'wins': 0, 'count': 0})
+                    newstat['count'] += 1
+                    if is_win:
+                        newstat['wins'] += 1
+
+            total = wins + losses
+            wr = int(wins / total * 100)
+            meta['title'] = f'{summoner.name} is {wins} wins and {losses} losses in the past {wins + losses} games. {wr}% WR.'
+            champions = list(champions.items())
+            champions.sort(key=lambda x: -x[1]['count'])
+            champions = champions[:3]
+            top_played = [f'{x[0]} - {x[1]["count"]}({int(x[1]["wins"] / x[1]["count"] * 100)}% WR)' for x in champions]
+            top_played = ', '.join(top_played)
+            meta['description'] = f'Top played: {top_played}'
+            icon = summoner.get_profile_icon()
+            if icon:
+                meta['image'] = icon.image_url()
+
     return meta
 
 
