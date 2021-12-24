23b9404833dd64e4b6a80992c36496a364dc5f06
Brian Perrett <bperrett@fusionbox.com>
2021-12-23 18:53:39 -0800

    Edit requirements

--- react/src/constants/mdparser.js
+++ react/src/constants/mdparser.js
@@ -2,7 +2,7 @@ import MarkdownIt from 'markdown-it'
 import markdownItLatex from 'markdown-it-latex'
 import 'markdown-it-latex/dist/index.css'
 import hljs from 'highlight.js'
-import 'highlight.js/styles/gruvbox-light.css'
+import 'highlight.js/styles/atom-one-dark.css'
 
 const mdParser = new MarkdownIt({
     highlight: function (str, lang) {
