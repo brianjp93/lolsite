9fa6e1b0b56d874121369d75e4eaba8ca0c7ce3e
Brian Perrett <perrettbrian@gmail.com>
2022-02-06 03:00:07 -0700

    set user

--- lolsite/settingsenv/circleci_settings.py
+++ lolsite/settingsenv/circleci_settings.py
@@ -17,7 +17,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "cirecle_test",
-        "USER": "postgres",
+        "USER": "root",
         "PASSWORD": "blah",
         "HOST": "127.0.0.1",
         "PORT": "5432",
