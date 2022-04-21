85e086c6a510b48054006739aed448c82003492b
Brian Perrett <perrettbrian@gmail.com>
2022-04-21 16:03:23 -0600

    Fix staticfiles dirs

--- lolsite/settings.py
+++ lolsite/settings.py
@@ -130,9 +130,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "lolsite", "static"),
-]
+STATICFILES_DIRS = []
 try:
     STATICFILES_DIRS += [
         os.path.join(BASE_DIR, "react", "build", "static"),
