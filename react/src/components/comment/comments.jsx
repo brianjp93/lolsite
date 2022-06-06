a2def562940ec7cfd9a356480d4e098739da0c07
Brian Perrett <perrettbrian@gmail.com>
2022-06-05 20:28:35 -0600

    Delete unused import.

--- react/src/components/comment/comments.jsx
+++ react/src/components/comment/comments.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { CreateComment } from './createcomment'
 import { ViewComments } from './viewcomments'
 import api from '../../api/api'
