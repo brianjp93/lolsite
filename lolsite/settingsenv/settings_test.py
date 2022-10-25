8a9d084de91715ee063d1eb8044b3ca79c78d2cb
Brian Perrett <perrettbrian@gmail.com>
2022-10-24 19:17:33 -0600

    Try to setup github tests.

--- lolsite/settingsenv/settings_test.py
+++ lolsite/settingsenv/settings_test.py
@@ -5,22 +5,23 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+
 REACT_DEV = False
 DEV = False
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "192.168.0.24"]
+ALLOWED_HOSTS = ["localhost"]
 BASE_URL = "http://localhost:8000"
 
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "cirecle_test",
-        "USER": "root",
-        "PASSWORD": "blah",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": os.getenv('DB'),
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv('DB_HOST'),
+        "PORT": os.getenv('DB_PORT'),
     }
 }
 
