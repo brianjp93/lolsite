cace53376a8399c1d521b5a55ef94ff9b6d45b05
Brian Perrett <perrettbrian@gmail.com>
2022-10-24 20:55:18 -0600

    nvm that doesn't do anything

--- lolsite/settingsenv/settings_test.py
+++ lolsite/settingsenv/settings_test.py
@@ -28,8 +28,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATICFILES_DIRS = []
-
 
 CELERY_BROKER_URL = "redis://localhost"
 
