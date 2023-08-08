d897fc86c5bf4e6f5a6b5fb63066f41e3bfb8389
Brian Perrett <bperrett@fusionbox.com>
2023-08-07 22:51:11 -0700

    Fix base logger

--- core/parsers.py
+++ core/parsers.py
@@ -7,8 +7,8 @@ logger = logging.getLogger(__name__)
 
 class BaseModelWithLogger(BaseModel, extra=Extra.allow):
     @model_validator(mode="after")
-    def log_extra(self, data):
+    def log_extra(self):
         extra = self.__pydantic_extra__
         if extra:
             logger.info(f'Extra fields detected on {self.__class__.__name__}: {extra}')
-        return data
+        return self
