4c2990c5cd0754cb6aacf7720e2a6674a009b2b1
Brian Perrett <perrettbrian@gmail.com>
2024-07-23 19:46:19 -0700

    Add alpine and item component

--- lolsite/urls.py
+++ lolsite/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from player import views as player_views
+from lolsite import views
 from django.conf.urls.static import static
 from django.conf import settings
-from django.http import HttpResponse
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("lolsite.urlsapi")),
     path("", include("player.urls")),
-    path("", lambda _: HttpResponse('Hello world.')),
+    path("", views.Home.as_view()),
     path("login/go/", player_views.login_action),
     path("logout/", player_views.logout_action, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
