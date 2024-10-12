d5d96687a17441ff6dd7ff0433a0fe2d9c649344
Brian Perrett <perrettbrian@gmail.com>
2024-10-12 12:22:07 -0700

    I'm an idiot

--- lolsite/periodic_tasks.py
+++ lolsite/periodic_tasks.py
@@ -12,7 +12,7 @@ app.conf.beat_schedule = {
     },
     "mt-huge-match-import": {
         "task": "match.tasks.huge_match_import_task",
-        "schedule": crontab(hour="0,12"),
+        "schedule": crontab(hour="0,12", minute="0"),
     }
 }
 app.conf.timezone = "America/Los_Angeles"  # type: ignore
