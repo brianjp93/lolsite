415f324387a046e3faeb75d78a67df3cebc73d93
Brian Perrett <bperrett@fusionbox.com>
2024-10-15 16:41:48 -0700

    Smaller task pool

--- match/tasks.py
+++ match/tasks.py
@@ -414,7 +414,7 @@ def huge_match_import_task(hours_thresh=72, exclude_hours=24, break_early=True):
             summoner.huge_match_import_at = import_time
             summoner.save(update_fields=["huge_match_import_at"])
 
-    for _ in celery_task_pool(get_jobs(qs.iterator(2000), thresh), pool_size=15):
+    for _ in celery_task_pool(get_jobs(qs.iterator(2000), thresh), pool_size=10):
         i += 1
         if i % 100 == 0:
             elapsed = time.perf_counter() - elapsed_start
