be24921c4c3bba1b8288aa40c6bf086d477fe2a4
Brian Perrett <perrettbrian@gmail.com>
2025-01-21 19:54:45 -0800

    add ability to refresh heartrate

--- activity/urls.py
+++ activity/urls.py
@@ -6,4 +6,5 @@ app_name = 'activity'
 urlpatterns = [
     path("integrations/", views.IntegrationsListView.as_view(), name="integrations"),
     path("<slug:code>/callback/", views.IntegrationCallbackView.as_view(), name="integration-callback"),
+    path("heartrate/refresh/<slug:match_id>/", views.update_heartrate, name="update-heartrate"),
 ]
